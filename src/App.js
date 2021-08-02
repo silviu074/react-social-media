21ccd202185e01b15fc09b32e42408f55c2156f5
Silviu <silviu074@gmail.com.com>
2021-08-02 18:38:50 +0300

    your message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Login, Register, Newsfeed } from "./pages";
+import { useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
+
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
-        <Route path=" " component />
+        <Route path="/" exact component={Newsfeed} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </Router>
   );
